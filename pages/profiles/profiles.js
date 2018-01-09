cad5adf05815abbdfee30d86a4bafef5c87bf6f1
aragakiyui <heng8050lee@foxmail.com>
2018-01-09 16:46:52 +0800

    initilize commit

---
+++ pages/profiles/profiles.js
@@ -0,0 +1,74 @@
+// pages/profiles/profiles.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    "name": "AragakiYui",
+    "email": "heng8050lee@foxmail.com",
+    "phone": "15623054630"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+
+  setName: function () {
+    this.setData({
+      "name": "chenfangfang"
+    })
+  }
+})
